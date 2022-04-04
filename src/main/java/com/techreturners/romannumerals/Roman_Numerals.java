f689a6ffe5d26550c82b3cf801e4aaec1af7086b
deepatesting <kspdeepa82@gmail.com>
2022-04-04 22:00:29 +0100

    Tried logic for more inputs.

--- src/main/java/com/techreturners/romannumerals/Roman_Numerals.java
+++ src/main/java/com/techreturners/romannumerals/Roman_Numerals.java
@@ -1,8 +1,93 @@
 package main.java.com.techreturners.romannumerals;
 
+
 public class Roman_Numerals {
 
-    public int convertRomanToInteger(String str){
+    public enum ConversionList {
+        One("I", 1),
+        Five("V", 5),
+        Ten("X", 10);
+
+        private String romanName = "";
+        private int number = 0;
+
+        ConversionList(String romanName, int number) {
+            this.romanName = romanName;
+            this.number = number;
+        }
+        public String getRomanName() {
+            return romanName;
+        }
+
+        public int getNumeral() {
+            return number;
+        }
+    }
+
+    //PART - I
+    public String convertNumberToRoman(int number){
+
+        String resStr = "";
+        int previousVal = 0;
+        int currentVal = 0;
+        String previousName = "";
+        String currentName = "";
+
+        for (ConversionList LIST_MAP : ConversionList.values()) {
+
+            currentVal = LIST_MAP.getNumeral();
+            currentName = LIST_MAP.getRomanName();
+
+            int diff  = number - previousVal;
+            //System.out.print(previousVal + diff);
+
+            if ( (number == 1) || (number == 5) || (number == 10) ){
+                if ( currentVal ==  number) {
+                    resStr = currentName;
+                    System.out.print(resStr + currentVal);
+                    break;
+                }
+            }
+            else if  (( previousVal < number) && ( previousVal != 0)) {
+
+                if (diff == 1) {
+                    resStr = previousName + resStr;
+                }
+
+                else {
+                    for (int i = 1; i <=number; i++) {
+                        resStr = resStr + previousName;
+                        System.out.print("I am");
+                    }
+
+                }
+                System.out.print(resStr);
+            }
+
+            previousVal = LIST_MAP.getNumeral();
+            previousName = LIST_MAP.getRomanName();
+        }
+
+        return resStr;
+
+        /*
+        switch(number){
+            case 1:
+                return "I";
+            case 5:
+                return "V";
+            case 10:
+                return "X";
+            default:
+                return "Wrong, Please enter a correct number";
+        }
+        */
+
+
+    }
+
+    //PART - II
+    public int convertRomanToNumber(String str){
         if (str.equals("I"))
             return 1;
         else return 0;
--- src/test/java/com/techreturners/romannumerals/Roman_NumeralsTest.java
+++ src/test/java/com/techreturners/romannumerals/Roman_NumeralsTest.java
@@ -1,23 +1,67 @@
 package test.java.com.techreturners.romannumerals;
 
+import main.java.com.techreturners.romannumerals.ConversionList;
 import main.java.com.techreturners.romannumerals.Roman_Numerals;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Roman_NumeralsTest {
 
-    private int actualResult;
+    private int intActualResult;
+    private String strActualResult;
 
-
+    //PART - I
     @Test
-    public void checkForRomanNumeralOne(){
+    public void checkForNumberToRomanOne(){
+        //Arrange
+        Roman_Numerals romanNumerals = new Roman_Numerals();
+        //ConversionList numberOne = ConversionList.I;
+
+        //Act
+        strActualResult = romanNumerals.convertNumberToRoman(4);
+
+        //Assert
+        Assertions.assertEquals("IV", strActualResult);
+    }
+
+    /*
+    @Test
+    public void checkForNumberToRomanTwo(){
         //Arrange
         Roman_Numerals romanNumerals = new Roman_Numerals();
 
         //Act
-        actualResult = romanNumerals.convertRomanToInteger("I");
+        strActualResult = romanNumerals.convertNumberToRoman(2);
 
         //Assert
-        Assertions.assertEquals(1, actualResult);
+        Assertions.assertEquals("II", strActualResult);
     }
+
+    @Test
+    public void checkForNumberToRomanFail(){
+        //Arrange
+        Roman_Numerals romanNumerals = new Roman_Numerals();
+
+        //Act
+        strActualResult = romanNumerals.convertNumberToRoman(0);
+
+        //Assert
+        Assertions.assertEquals("Wrong, Please enter a correct number", strActualResult);
+    }
+
+
+    // PART - II
+    @Test
+    public void checkForConvertRomanToNumberOne(){
+        //Arrange
+        Roman_Numerals romanNumerals = new Roman_Numerals();
+
+        //Act
+        intActualResult = romanNumerals.convertRomanToNumber("I");
+
+        //Assert
+        Assertions.assertEquals(1, intActualResult);
+    }
+    */
+
 }
